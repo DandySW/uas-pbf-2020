f9d07178e03023f8c906d5c164637f158edfaca2
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-12-18 15:59:46 +0700

    commit pertama

---
+++ resources/views/template/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+@include('template.header')
+
+<body>
+
+@include('template.navbar')
+
+    @yield('content')
+
+@include('template.footer')
+
+@include('template.script')
+
+</body>
+</html>

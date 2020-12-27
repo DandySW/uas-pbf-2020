075d1d29f9421bd501db052026719de2af121b63
Dandy Satrio <dandysatrio123@gmail.com>
2020-12-28 02:49:47 +0700

    mengedit model untuk user

--- app/Product.php
+++ app/Product.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'prod_name',
-        'category_id',
-        'stock',
-        'price',
-        'description'
+        'prod_name', 'category_id', 'stock', 'price', 'description'
     ];
 
     public $timestamps = false;
--- app/Product_Image.php
+++ app/Product_Image.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product_Image extends Model
 {
     protected $fillable = [
-        'product_id',
-        'image_path'
+        'product_id', 'image_path'
     ];
 }
